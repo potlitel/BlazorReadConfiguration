56fbc76eb579483602cc170059952eadc9f1aa4e
potlitel <potlitel@gmail.com>
2023-11-02 09:14:41 -0400

    Se logra leer info de usuario.

---
+++ BlazorReadConfiguration/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorReadConfiguration.Models
+{
+    public class User
+    {
+        public string Id { get; set; } = null!;
+        public string Username { get; set; } = null!;
+
+        public string Mail { get; set; } = null!;
+
+        public string Profile { get; set; } = null!;
+    }
+}
